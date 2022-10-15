3b2d99799b017c78338066614860f55fb1f27ee2
Gili Yankovitch <giliy@nyxsecurity.net>
2022-10-15 17:57:56 +0300

    Fixing paths in flash script

--- Digistump/bootloader/Burn_AT85_bootloader.bat
+++ Digistump/bootloader/Burn_AT85_bootloader.bat
@@ -1,2 +1,2 @@
-C:\Users\Gili\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\bin\avrdude.exe -C"C:\Users\Gili\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\etc\avrdude.conf" -v -pattiny85 -cstk500v1 -PCOM10 -b19200 -Uflash:w:"C:\Users\Gili\Downloads\Digistump Drivers\ATTiny85\ATtiny85-Boot-loader-main\ATtiny85.hex":i
+%HOMEPATH%\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\bin\avrdude.exe -C"%HOMEPATH%\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\etc\avrdude.conf" -v -pattiny85 -cstk500v1 -PCOM10 -b19200 -Uflash:w:"%HOMEPATH%\Downloads\Digistump Drivers\ATTiny85\ATtiny85-Boot-loader-main\ATtiny85.hex":i -U lfuse:w:0xe1:m -U hfuse:w:0xdd:m -U efuse:w:0xfe:m
 @pause
