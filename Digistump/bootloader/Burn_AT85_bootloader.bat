a11e97e3d1ed3d37b19ef369fc48e82e8052d44b
Gili Yankovitch <giliy@nyxsecurity.net>
2022-10-15 18:20:07 +0300

    Update Burn_AT85_bootloader.bat

--- Digistump/bootloader/Burn_AT85_bootloader.bat
+++ Digistump/bootloader/Burn_AT85_bootloader.bat
@@ -1,2 +1,2 @@
-%HOMEPATH%\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\bin\avrdude.exe -C"%HOMEPATH%\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\etc\avrdude.conf" -v -pattiny85 -cstk500v1 -PCOM10 -b19200 -Uflash:w:"%HOMEPATH%\Downloads\Digistump Drivers\ATTiny85\ATtiny85-Boot-loader-main\ATtiny85.hex":i -U lfuse:w:0xe1:m -U hfuse:w:0xdd:m -U efuse:w:0xfe:m
+%HOMEPATH%\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\bin\avrdude.exe -C"%HOMEPATH%\Documents\ArduinoData\packages\arduino\tools\avrdude\6.3.0-arduino17\etc\avrdude.conf" -v -pattiny85 -cstk500v1 -PCOM10 -b19200 -Uflash:w:"ATtiny85.hex":i -U lfuse:w:0xe1:m -U hfuse:w:0xdd:m -U efuse:w:0xfe:m
 @pause
